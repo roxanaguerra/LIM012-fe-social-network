b3b61a4cc140a9b522600639ad5b984dcaa823f1
RossEvv <ros22ch.caceres@gmail.com>
2020-05-26 10:59:04 -0500

    maquetación

--- src/view/register.js
+++ src/view/register.js
@@ -1,14 +1,15 @@
-const register = `<div>
-  <form id="sign-up" action="">
-    <input type="" id="input-name" class="login-input" placeholder="Nombre">
-    <input type="" id="input-mail" class="login-input" placeholder="ejemplo@mail.com">
-    <input type="" id="input-password" class="" placeholder="Escribe tu contraseña" >
-    <button class="" id="btn-registrar" data-action ='login'>Registrar</button>
-      <p class="">O bien ingresar con...</p>
-        <button class="" id="btn-google">G</button>
-        <button class="" id="btn-facebook">F</button>
-    <p id="ms-iniciar-registrar" class="">¿Tienes cuenta? <a href= "#/registro" class="" id="btn-change-iniciar-registrar">Inicia sesión</a></p>
-  </form>
-  </div>`;
+// const register = `<div>
+//   <form id="sign-up" action="">
+//     <input type="" id="input-name" class="login-input" placeholder="Nombre">
+//     <input type="" id="input-mail" class="login-input" placeholder="ejemplo@mail.com">
+//     <input type="" id="input-password" class="" placeholder="Escribe tu contraseña" >
+//     <button class="" id="btn-registrar" data-action ='login'>Registrar</button>
+//       <p class="">O bien ingresar con...</p>
+//         <button class="" id="btn-google">G</button>
+//         <button class="" id="btn-facebook">F</button>
+//     <p id="ms-iniciar-registrar" class="">¿Tienes cuenta?
+//  <a href= "#/registro" class="" id="btn-change-iniciar-registrar">Inicia sesión</a></p>
+//   </form>
+//   </div>`;
 
-const signForm = register;
+// const signForm = register;
