ee1e99d01fe40e4e371fafc1fcd80eb481bf71d7
roxanaguerra <sguerraapaza@gmail.com>
2020-05-27 13:07:41 -0500

    register view

--- src/view/register.js
+++ src/view/register.js
@@ -1,15 +1,39 @@
-// const register = `<div>
-//   <form id="sign-up" action="">
-//     <input type="" id="input-name" class="login-input" placeholder="Nombre">
-//     <input type="" id="input-mail" class="login-input" placeholder="ejemplo@mail.com">
-//     <input type="" id="input-password" class="" placeholder="Escribe tu contraseña" >
-//     <button class="" id="btn-registrar" data-action ='login'>Registrar</button>
-//       <p class="">O bien ingresar con...</p>
-//         <button class="" id="btn-google">G</button>
-//         <button class="" id="btn-facebook">F</button>
-//     <p id="ms-iniciar-registrar" class="">¿Tienes cuenta?
-//  <a href= "#/registro" class="" id="btn-change-iniciar-registrar">Inicia sesión</a></p>
-//   </form>
-//   </div>`;
+export default () => {
+    const viewRegister = `
+    <div class="ctn-register-login">
+    <div class="content flex column">
+      <div class="">
+        <h1 class="delivery-drone">Delivery Drone</h1>
+        <h6 class="safetyHome"> STAY HOME, STAY SAFE</h6>
+      </div>
+      <div class="container-form flex column">
+        <div class="data-register flex column">
+          <div class="inputs-form">
+            <input class="name" id="username" type="text" placeholder="Nombres y Apellidos">
+            <input class="email" id="email" type="mail" placeholder="E-mail">
+            <input class="password" id="password" type="text" placeholder="Password">
+          </div>
+          <button class="btn-register" id="register">Register</button>
+        </div>
+        <p class="txt-register">Or</p>
+        <div class="options-register">
+          <img class="logo-fb" src="assets/fb.png" alt="">
+          <img class="logo-google" src="assets/gg.png" alt="">
+        </div>
+        <div class="ask-option flex">
+          <p class="question" id="comment-register">If you have an account</p>
+          <a class="option" id="comment-signin" href="#/">Sign In</a>
+        </div>
+      </div>
 
-// const signForm = register;
+      <h4 class="find-delivers">FIND DELIVERIES TO <br> YOU SAFELY</h4>
+    </div>
+  </div>`;
+
+    const divElemt = document.createElement('div');
+    // divElemt.classList.add('position')
+    divElemt.innerHTML = viewRegister;
+    return divElemt;
+
+    
+};
