561bbaf2a0058acca7e2f59d389a01e9e97200bb
greciagarciav <garciav.grecia@gmail.com>
2020-05-27 18:44:09 -0500

    fix: fix linter errors

--- src/view-controller/router.js
+++ src/view-controller/router.js
@@ -1,18 +1,19 @@
-import { components } from '../view/view-index.js'
-const changeView = (route) => {
-    // console.log(route);
-    // const id = hash.split('/')[1];
-    const container = document.getElementById('general-container');
-    container.innerHTML = '';
-    switch (route) {
-        case '':
-        case '#':
-        case '#/': { return container.appendChild(components.login()); }
-        case '#/register': { return container.appendChild(components.register()); }
-        case '#/home': { return container.appendChild(components.home()); }
-        default:
-            return container.appendChild(components.error())
-    }
-}
+import { components } from '../view/view-index.js';
 
-export {changeView}
+const changeView = (route) => {
+  // console.log(route);
+  // const id = hash.split('/')[1];
+  const container = document.getElementById('general-container');
+  container.innerHTML = '';
+  switch (route) {
+    case '':
+    case '#':
+    case '#/': { return container.appendChild(components.login()); }
+    case '#/register': { return container.appendChild(components.register()); }
+    case '#/home': { return container.appendChild(components.home()); }
+    default:
+      return container.appendChild(components.error());
+  }
+};
+
+export { changeView };
