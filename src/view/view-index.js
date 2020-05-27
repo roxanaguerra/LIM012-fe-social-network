997d8639825a38ac4515a2f90390a5fe67d02d06
roxanaguerra <sguerraapaza@gmail.com>
2020-05-27 13:15:52 -0500

    new file(view-index.js) principal de vistas

---
+++ src/view/view-index.js
@@ -0,0 +1,13 @@
+import Home from './home.js'
+import Register from './register.js'
+import Login from './login.js'
+import errorMessage from './error.js'
+
+const components = {
+    home: Home,
+    register: Register,
+    login: Login,
+    error: errorMessage,
+}
+
+export { components };
