f97fd2e21b2b13f226c5a1f8cf57d30d88b4455a
greciagarciav <garciav.grecia@gmail.com>
2020-05-27 18:43:25 -0500

    feat: add events

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
-import { changeView } from './view-controller/router.js'
-const init = ()=> {
-    changeView(window.location.hash)
-    window.addEventListener('hashchange', ()=> {
-        changeView(window.location.hash)
-    })
-}
+// import { SignInGoogle } from './controller/controller-authentication.js';
+import { changeView } from './view-controller/router.js';
+
+const events = () => {
+  const btnRegister = document.getElementById('btn-register');
+  const btnGoogle = document.getElementById('google');
+  btnRegister.addEventListener('click', () => {
+    // e.preventDefault();
+    console.log('registrado');
+  });
+
+  btnGoogle.addEventListener('click', () => {
+    // e.preventDefault();
+    console.log('registrado');
+    // SignInGoogle();
+  });
+};
+
+
+const init = () => {
+  changeView(window.location.hash);
+  window.addEventListener('hashchange', () => {
+    changeView(window.location.hash);
+    events();
+  });
+};
+
 window.addEventListener('load', init);
