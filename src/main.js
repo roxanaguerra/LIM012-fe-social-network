79699ee36d2ec98f5adfb6a77217c323238daaf2
roxanaguerra <sguerraapaza@gmail.com>
2020-05-27 23:36:52 -0500

    update file

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { changeView } from './view-controller/router.js'
 const init = ()=> {
     changeView(window.location.hash)
     window.addEventListener('hashchange', ()=> {
-        changeView(window.location.hash)
+        changeView(window.location.hash);
     })
 }
 window.addEventListener('load', init);
