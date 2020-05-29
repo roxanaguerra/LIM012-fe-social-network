d11522122b999eb4f4bdb46495bf2ef1010988e0
roxanaguerra <sguerraapaza@gmail.com>
2020-05-28 23:50:51 -0500

    update file

--- src/view/error.js
+++ src/view/error.js
@@ -2,8 +2,14 @@ export default () => {
     const viewDifferent = `
             <h2>404</h2>
             <h1>Página no encontrada</h1>
-            <p>El archivo especificado no se encontró en este sitio web. Por favor, compruebe la URL para errores y vuelva a intentarlo.</p>
-          `;
+            <p>El archivo especificado no se encontró en este sitio web. 
+            Por favor, compruebe la URL para errores y vuelva a intentarlo.</p>
+            
+            <div class="ask-option flex">
+            <p class="question" id="comment-register">PRUEBA ERROR - 404</p>
+            <a class="option" id="comment-signin" href="#/">Inicio</a>
+            </div>
+            `;
 
     const divElemt = document.createElement('div');
     divElemt.setAttribute('id', 'message');
