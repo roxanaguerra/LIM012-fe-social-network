a12efc7a96624583c8cce645ecbf00e2a50358f5
RossEvv <ros22ch.caceres@gmail.com>
2020-06-02 09:36:46 -0500

    agrega mensajes de error en el login

--- src/controller/controller-autentication.js
+++ src/controller/controller-autentication.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 /* eslint-disable import/no-cycle */
 import {
   signUp,
@@ -6,42 +7,61 @@ import {
   signInGoogle,
   signInFacebook,
 } from '../model/model-authentication.js';
-import { componentsView } from '../view/view-index.js';
+// import { componentsView } from '../view/view-index.js';
 
-
+const validateEmail = (email) => {
+  // para validar que ingrese un email de acuerdo a su sintáxis
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
 // REGISTRAR USUARIO
 export const registerNewUser = (emailRegister, passwordRegister) => {
   // const view = componentsView.login();
+  const span = document.querySelector('#span');
   signUp(emailRegister, passwordRegister)
     .then((userCredential) => {
       console.log('registradx', userCredential);
-      window.location.hash = '#/home';
+      window.location.hash = '#/';
     })
-    .catch((error) =>
-      // let message = '';
-      // if (error.code === 'auth/wrong-password') {
-      //   message = 'contraseña incorrecta';
-      // }
-      // const span = view.querySelector('#codeError');
-      // span.innerHTML = message;
-      error);
+    .catch((error) => {
+      console.log(error);
+      if (error.code === 'auth/invalid-email') {
+        span.innerHTML = 'Email incorrecto';
+      } else if (error.code === 'auth/weak-password') {
+        span.innerHTML = 'contraseña insegura Ingrese mínimo 6 caracteres';
+      }
+      setTimeout(
+        () => (span.innerHTML = 'El futuro es hoy...Regístrate'),
+        2000,
+      );
+    });
 };
 
 // INICIAR SESIÓN
 export const authSignIn = (emailLogin, passwordLogin) => {
-  const view = componentsView.login();
+  const span = document.querySelector('#span');
   signIn(emailLogin, passwordLogin)
     .then((userCredential) => {
-      console.log('Usted ya ingreso, ya esta logeado', userCredential);
+      console.log('Bienvenido otra vez', userCredential);
       window.location.hash = '#/home';
     })
     .catch((error) => {
       console.log(error);
       if (error.code === 'auth/wrong-password') {
-        const span = view.querySelector('#span');
-        alert('soy un error');
-        span.innerHTML = 'soy un error';
+        span.innerHTML = 'Contraseña inválida';
+      } else if (error.code === 'auth/invalid-email') {
+        // span.innerHTML = '';
+        span.innerHTML = 'Correo electrónico incorrecto. Intente otra vez';
+      } else if (error.code === 'auth/user-not-found') {
+        // span.innerHTML = '';
+        span.innerHTML = 'Usario no registrado';
+      } else if (error.code === 'auth/too-many-requests') {
+        span.innerHTML = 'Refresque la página';
       }
+      setTimeout(
+        () => (span.innerHTML = 'Muchos envíos te esperan...'),
+        2000,
+      );
     });
 };
 
@@ -78,24 +98,32 @@ export const authSignInFacebook = () => {
     .then((result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
       const token = result.credential.accessToken;
+      console.log(token);
       // The signed-in user info.
       const user = result.user;
       window.location.hash = '#/home';
       console.log('Facebook Sign In');
     })
     .catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      const credential = error.credential;
-      console.log('Error Facebook Prueba');
-      console.log(errorCode);
-      console.log(errorMessage);
-      console.log(email);
-      console.log(credential);
+      console.log(error);
+      firebase.auth().fetchProvidersForEmail(error.email)
+  .then(providers => {
+    //providers returns this array -> ["google.com"]
+    // You need to sign in the user to that google account
+    // with the same email.
+    // In a browser you can call:
+    // var provider = new firebase.auth.GoogleAuthProvider();
+    // provider.setCustomParameters({login_hint: error.email});
+    // firebase.auth().signInWithPopup(provider)
+    // If you have your own mechanism to get that token, you get it
+    // for that Google email user and sign in
+    firebase.auth().signInWithCredential(googleCred)
+      .then(user => {
+        // You can now link the pending credential from the first
+        // error.
+        user.linkWithCredential(error.credential)
+      })
+      .catch(error => log(error))
     });
 };
 
