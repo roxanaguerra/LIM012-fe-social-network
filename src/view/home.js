48b0fd3e7ed9128b06d417eb6cb0c0a3a27d6888
roxanaguerra <sguerraapaza@gmail.com>
2020-06-04 10:46:20 -0500

    feat: diseño y publicacion de post

--- src/view/home.js
+++ src/view/home.js
@@ -1,3 +1,8 @@
+import {
+  postCreado,
+  postRead,
+} from '../controller/controller-posts.js';
+
 export default () => {
   const viewHome = `
   <div class="mobile-container">
@@ -26,7 +31,7 @@ export default () => {
                 </div>
               </div>
               <div class="ctn-txt-post flex">
-                <textarea name="" id="" cols="45" rows="4" placeholder="What's on your mind?"></textarea>
+                <textarea id="input-post" cols="45" rows="4" placeholder="What's on your mind?"></textarea>
               </div>  
             </div>
             <div class="ctn-post-status ctn flex">
@@ -41,8 +46,10 @@ export default () => {
                   <img src="assets/option.png" alt="">
                 </span>
               </div>
-              <button class="btn-post">POST</button>
+              <button class="btn-post" id="btn-post">POST</button>
             </div>
+    </div>
+    <div id="new-post" class="">
     </div>
               
           <!-- Past photo posts -->
@@ -105,73 +112,84 @@ export default () => {
             </div>  
           </div>
       
-          <!-- Past text posts -->
-          <div class="ctn-text-posted flex column margin-top">
-            <div class="ctn start">
-              <div class="ctn-post-details flex">
-                <div class="ctn-img-post">
-                  <img class="border" src="assets/profile-photo.jpg" alt="">
-                </div>
-                <div class="">
-                  <a class="username" href="">Ana Wong</a>
-                  <div class="ctn-post-status flex">
-                    <span>23/05/2020</span>
-                    <span>18:00</span>
-                  </div>
-                </div>
-                <div class="config-post">
-                  <img src="assets/public.png" alt="">
-                  <span class="config-privacy">
-                    <img src="assets/option.png" alt="">
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div class="txt-posted flex">
-              <p>eatae illum, architecto commodi expedita eos velit voluptatem iusto vel.</p>
-            </div>
-            <div class="ctn-likes-comments ctn flex">
-              <div class="likes flex">
-                <img src="assets/like.png" alt="">
-                <span>15 Likes</span>
-              </div>
-              <div class="comments flex">
-                <img src="assets/comment.png" alt="">
-                <span>5 comments</span>
-              </div>
-            </div>
-            <div class="ctn flex ctn-create-comment  start">
-              <div class="flex">
-                <div class="ctn-img-post">
-                  <img src="assets/profile-photo.jpg" alt="">
-                </div>
-                <div class="ctn-comment-txa">
-                  <textarea name="" id="" cols="37" rows="2" placeholder="Add a comment"></textarea>
-                </div>
-              </div> 
-            </div>
-            <div class="ctn ctn-last-comment start">
-              <div class="ctn-post-details flex">
-                <div class="ctn-img-post">
-                  <img src="assets/comment-photo.jpg" alt="">
-                </div>
-                <div class="ctn-comment">
-                  <a class="username-comment" href="">Eva Alva</a>
-                  <div class="comment">
-                      <p class="">Increible vista me encantan los drones</p>                            
-                  </div>
-                </div>
-              </div>
-            </div>            
-          </div>
-  </div>
-  <div class="ask-option flex">
-        <p class="question" id="comment-register">HOME PRUEBA</p>
-        <a class="option" id="comment-signin" href="#/">login</a>
-    </div>
+         
   `;
+
   const divElemt = document.createElement('div');
-  // divElemt.classList.add('position')
   divElemt.innerHTML = viewHome;
+
+  const btnPost = divElemt.querySelector('#btn-post');
+  btnPost.addEventListener('click', () => {
+    const inputPost = divElemt.querySelector('#input-post').value;
+    console.log('prueba: ', inputPost);
+    console.log(inputPost);
+    divElemt.querySelector('#input-post').value = '';
+    postCreado(inputPost);
+    postRead();
+    const divPostNew = divElemt.querySelector('#new-post');
+    divPostNew.innerHTML += `
+  <!-- Past text posts -->
+  <div class="ctn-text-posted flex column margin-top">
+    <div class="ctn start">
+      <div class="ctn-post-details flex">
+        <div class="ctn-img-post">
+          <img class="border" src="assets/profile-photo.jpg" alt="">
+        </div>
+        <div class="">
+          <a class="username" href="">Ana Wong</a>
+          <div class="ctn-post-status flex">
+            <span>23/05/2020</span>
+            <span>18:00</span>
+          </div>
+        </div>
+        <div class="config-post">
+          <img src="assets/public.png" alt="">
+          <span class="config-privacy">
+            <img src="assets/option.png" alt="">
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="txt-posted flex">
+      <p>${inputPost} </p>
+    </div>
+    <div class="ctn-likes-comments ctn flex">
+      <div class="likes flex">
+        <img src="assets/like.png" alt="">
+        <span>15 Likes</span>
+      </div>
+      <div class="comments flex">
+        <img src="assets/comment.png" alt="">
+        <span>5 comments</span>
+      </div>
+    </div>
+    <div class="ctn flex ctn-create-comment  start">
+      <div class="flex">
+        <div class="ctn-img-post">
+          <img src="assets/profile-photo.jpg" alt="">
+        </div>
+        <div class="ctn-comment-txa">
+          <textarea name="" id="" cols="37" rows="2" placeholder="Add a comment"></textarea>
+        </div>
+      </div> 
+    </div>
+    <div class="ctn ctn-last-comment start">
+      <div class="ctn-post-details flex">
+        <div class="ctn-img-post">
+          <img src="assets/comment-photo.jpg" alt="">
+        </div>
+        <div class="ctn-comment">
+          <a class="username-comment" href="">Eva Alva</a>
+          <div class="comment">
+              <p class="">Increible vista me encantan los drones</p>                            
+          </div>
+        </div>
+      </div>
+    </div>            
+  </div>
+</div>
+  `;
+  });
+
   return divElemt;
 };
