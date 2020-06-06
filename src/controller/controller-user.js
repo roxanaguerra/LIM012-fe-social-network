2fe7939c74e281038dfa7ebc41516dd1c8f82402
greciagarciav <garciav.grecia@gmail.com>
2020-06-06 17:51:55 -0500

    docs-feat: create doc for controller user, create function to read user profile

---
+++ src/controller/controller-user.js
@@ -0,0 +1,22 @@
+import { userProfile } from '../model/model-user.js';
+
+export const readUserProfile = (id) => {
+  userProfile(id)
+    .then((doc) => {
+      if (doc.exists) {
+        const userName = document.querySelector('.username');
+        const userPhoto = document.querySelector('.img-photo-post');
+        const userAbout = document.querySelector('.userabout');
+        const userCover = document.querySelector('.usercover');
+        userName.innerHTML = doc.data().username;
+        userPhoto.innerHTML = `<img class="border" src="${doc.data().profileImg}" alt="">`;
+        userAbout.innerHTML = doc.data().about;
+        userCover.innerHTML += `<img src="${doc.data().coverImg}" alt="">`;
+      } else {
+        console.log('this doc does not exists');
+      }
+    })
+    .catch(() => {
+
+    });
+};
