d9134f5e43147d59f09cc2bacb38996d7f7fe5ac
greciagarciav <garciav.grecia@gmail.com>
2020-06-06 17:25:30 -0500

    feat: add more properties for postCreado

--- src/controller/controller-posts.js
+++ src/controller/controller-posts.js
@@ -3,11 +3,14 @@ import {
   readPostPrueba,
 } from '../model/model-posts.js';
 
-export const postCreado = (inputPost) => {
+export const postCreado = (inputPost, id, name, mode, like) => {
   allPostPrueba().add({
-    first: inputPost,
-    date: '03/05/2020',
-    idUser: 'roxana333',
+    post: inputPost,
+    date: new Date(),
+    idUser: id,
+    username: name,
+    privacy: mode,
+    likes: like,
   })
     .then((docRef) => {
       console.log('Document written with ID: ', docRef.id);
