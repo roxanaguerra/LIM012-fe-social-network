b12503c5c2c551ebbf15b4b0239d5b73ccf79fab
roxanaguerra <sguerraapaza@gmail.com>
2020-06-06 17:08:54 -0500

    modificando el observador

--- src/view/login.js
+++ src/view/login.js
@@ -3,6 +3,7 @@ import {
   authSignInGoogle,
   authSignInFacebook,
   signOutUser,
+  observador,
 } from '../controller/controller-autentication.js';
 
 export default () => {
@@ -51,25 +52,7 @@ export default () => {
     authSignIn(emailLogin, passwordLogin);
   });
 
-  const observador = () => {
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        console.log('Existe Usuario Activo');
-        // *********************
-        // User is signed in.
-        // const displayName = user.displayName;
-        // const email = user.email;
-        // const emailVerified = user.emailVerified;
-        // const photoURL = user.photoURL;
-        // const isAnonymous = user.isAnonymous;
-        // const uid = user.uid;
-        // const providerData = user.providerData;
-        // *********************
-      } else {
-        console.log('No existe Usuario Activo');
-      }
-    });
-  };
+  // Se llama a este observador cada vez que cambia el estado de acceso del usuario.
   observador();
 
   // CERRAR SESIÓN 'funcion para boton singOut'
