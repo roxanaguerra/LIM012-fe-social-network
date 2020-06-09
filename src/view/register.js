9c8ae83716bba18d693df85dc1e21da7c2b9ebc8
greciagarciav <garciav.grecia@gmail.com>
2020-06-08 20:33:25 -0500

    fix: fix register view

--- src/view/register.js
+++ src/view/register.js
@@ -2,42 +2,39 @@ import {
   registerNewUser,
   authSignInGoogle,
   authSignInFacebook,
-  // observador,
 } from '../controller/controller-autentication.js';
 
 export default () => {
   const viewRegister = `
-    <div class="ctn-register-login">
-    <div class="content flex column">
-      <div class="ctn-titles">
-        <h1 class="delivery-drone">Delivery Drone</h1>
-        <h6 class="safetyHome"> STAY HOME, STAY SAFE</h6>
+    <div class="content1 flex column">
+      <div>
+        <h1>Delivery Drone</h1>
+        <h6> STAY HOME, STAY SAFE</h6>
       </div>
       <div class="container-form flex column">      
         <div class="data-register flex column">
           <div class="inputs-form">
-            <input class="name" id="name-register" type="text" placeholder="Nombres y Apellidos">
-            <input class="email" id="email-register" type="email" placeholder="E-mail">
-            <input class="password" id="password-register" type="password" placeholder="Password">
+            <input id="name-register" type="text" placeholder="Nombres y Apellidos">
+            <input id="email-register" type="email" placeholder="E-mail">
+            <input id="password-register" type="password" placeholder="Password">
           </div>
           
           <button class="btn-form" id="btn-register">Register</button>
-          <span id="span"></span>
+          <span class="error-msg" id="span"></span>
         </div>
         <p class="txt-register">Or</p>
-        <div class="options-register">
+        <div>
           <img class="logo-fb" src="assets/fb.png" alt="" id="facebook-register">
           <img class="logo-google" src="assets/gg.png" alt="" id="google-register">
         </div>
         <div class="ask-option flex">
-          <p class="question" id="comment-register">If you have an account</p>
+          <p class="question">If you have an account</p>
           <a class="option" id="comment-signin" href="#/">Sign In</a>
         </div>
       </div>
-
       <h4 class="find-delivers">FIND DELIVERIES TO <br> YOU SAFELY</h4>
     </div>
-  </div>`;
+  `;
 
   const divElemt = document.createElement('div');
   divElemt.innerHTML = viewRegister;
@@ -46,11 +43,9 @@ export default () => {
   const btnRegister = divElemt.querySelector('#btn-register');
   btnRegister.addEventListener('click', (e) => {
     e.preventDefault(); // cancelar el evento de reinicio de formulario
-    // const nameRegister = divElemt.querySelector('#name-register').value;
     const emailRegister = divElemt.querySelector('#email-register').value;
     const passwordRegister = divElemt.querySelector('#password-register').value;
     console.log(emailRegister, passwordRegister);
-    // registerNewUser(nameRegister, emailRegister, passwordRegister);
     registerNewUser(emailRegister, passwordRegister);
   });
 
@@ -69,9 +64,6 @@ export default () => {
     console.log('Facebook Prueba');
     authSignInFacebook();
   });
-
-  // Se llama a este observador cada vez que cambia el estado de acceso del usuario.
-  // observador();
 
   return divElemt;
 };
