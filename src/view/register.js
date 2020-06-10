52f080fdbad3818ddf51ef27b0bda40cb2517ab9
greciagarciav <garciav.grecia@gmail.com>
2020-06-09 19:07:38 -0500

    feat: add validation when inputs of register are empty

--- src/view/register.js
+++ src/view/register.js
@@ -39,14 +39,38 @@ export default () => {
   const divElemt = document.createElement('div');
   divElemt.innerHTML = viewRegister;
 
+  const validateEmail = (email) => {
+    // para validar que ingrese un email de acuerdo a su sintaxis
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+
   // REGISTRAR USUARIO
   const btnRegister = divElemt.querySelector('#btn-register');
   btnRegister.addEventListener('click', (e) => {
     e.preventDefault(); // cancelar el evento de reinicio de formulario
+    const username = document.querySelector('#name-register').value;
     const emailRegister = divElemt.querySelector('#email-register').value;
     const passwordRegister = divElemt.querySelector('#password-register').value;
+    const validateSintaxEmail = validateEmail(emailRegister);
+    const span = document.querySelector('#span');
     console.log(emailRegister, passwordRegister);
-    registerNewUser(emailRegister, passwordRegister);
+    if (username === '') {
+      span.innerHTML = '*Debes ingresar Nombres y Apellidos';
+    } else if (emailRegister === '') {
+      span.innerHTML = '*Debes ingresar un correo';
+    } else if (passwordRegister === '') {
+      span.innerHTML = '*Debes ingresar una contraseña';
+    } else if (!validateSintaxEmail) {
+      span.innerHTML = '*Formato de correo inválido';
+    } else {
+      registerNewUser(emailRegister, passwordRegister);
+    }
+    setTimeout(
+      // eslint-disable-next-line no-return-assign
+      () => (span.innerHTML = ''),
+      7000,
+    );
   });
 
   // INICIO DE SESIÓN CON GOOGLE
