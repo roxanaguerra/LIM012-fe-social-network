3b51a9b3cddbb8c502cb736eb8646494048b9112
roxanaguerra <sguerraapaza@gmail.com>
2020-06-18 21:36:39 -0500

    fix: ordenando MVC, firebase.storage()

--- src/model/model-storage.js
+++ src/model/model-storage.js
@@ -1,17 +1,23 @@
 // FIREBASE - STORAGE
 // Get a reference to the storage service, which is used to create references in your storage bucket
-export const storage = firebase.storage();
+const storage = () => firebase.storage();
 
 // Create a storage reference from our storage service
-export const storageRef = storage.ref();
+const storageRef = () => storage.ref();
 
-// Create a child reference
-export const imagenRefChild = storageRef.child('photoPost');
+// // Create a child reference
+// const imagenRefChild = storageRef.child('photoPost');
 
 // FUNCION PARA ELIMINAR LA IMG DEL STORAGE, ANTES DE AGREGAR A LA COLECCION POST
-export const deleteFileStorage = (file, uid) => storageRef.child(`photoPost/${uid}/${file}`).delete()
+const deleteFileStorage = (file, uid) => storageRef.child(`photoPost/${uid}/${file}`).delete()
   .then(() => {
     console.log('Se elimino la Imagen!');
   }).catch((error) => {
     console.log('Error al eliminar!', error);
   });
+
+export default {
+  storage,
+  storageRef,
+  deleteFileStorage,
+};
