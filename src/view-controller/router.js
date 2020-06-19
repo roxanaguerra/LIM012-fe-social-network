25dd8eada3d37d2e8c15209ca95cb9b29e0f5f76
roxanaguerra <sguerraapaza@gmail.com>
2020-06-18 21:55:31 -0500

    fix: importando models

--- src/view-controller/router.js
+++ src/view-controller/router.js
@@ -1,15 +1,16 @@
 import { componentsView } from '../view/view-index.js';
-import { signOutUser } from '../controller/controller-autentication.js';
+import { models } from '../model/model-index.js';
 
 const container = document.getElementById('general-container');
 
+// OBSERVADOR
 const authenticate = (view) => {
   let html = '';
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       html = container.appendChild(view());
     } else {
-      signOutUser();
+      models.signOutUser();
       window.location.hash = '#/';
     }
   });
@@ -17,17 +18,16 @@ const authenticate = (view) => {
 };
 
 const changeView = (route) => {
-  // console.log(route);
   container.innerHTML = '';
   switch (route) {
     case '':
     case '#':
     case '#/': {
-      signOutUser();
+      models.authentication.signOutUser();
       return container.appendChild(componentsView.login());
     }
     case '#/register': {
-      signOutUser();
+      models.authentication.signOutUser();
       return container.appendChild(componentsView.register()); }
     case '#/home':
       // eslint-disable-next-line max-len
@@ -35,7 +35,7 @@ const changeView = (route) => {
     case '#/profile':
       return authenticate(componentsView.profile);
     default:
-      return container.appendChild(componentsView.error());
+      return container.appendChild(componentsView.errorMessage());
   }
 };
 
